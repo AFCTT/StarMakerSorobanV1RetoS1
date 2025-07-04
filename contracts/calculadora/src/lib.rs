b93b11cf3a80e71c4fd913676c28207afecc4ba4
Andres Centanaro <centanarot-afelipe@javeriana.edu.co>
2025-07-04 17:40:40 -0500

    Actividad_check

--- contracts/calculadora/src/lib.rs
+++ contracts/calculadora/src/lib.rs
@@ -1,23 +1,27 @@
 #![no_std]
 
-
-use soroban_sdk::{contract, contractimpl,  Env, symbol_short, Symbol};
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol};
 const RESULTADO: Symbol = symbol_short!("RESULTADO");
 #[contract]
 pub struct Contract;
 
-
 #[contractimpl]
 impl Contract {
-    
-    pub fn sumar(env: Env, a:i128, b:i128) -> i128 {
-      //Implementar función que sume dos números
-      return 30;
+    pub fn sumar(env: Env, a: i128, b: i128) -> i128 {
+        //Implementar función que sume dos números
+        let resultado = a + b;
+        env.storage().persistent().set(&RESULTADO, &resultado);
+        resultado;
+        return 30;
     }
 
     pub fn resultado_anterior(env: Env) -> i128 {
-           //Implementar función que retorne el valor anterior
-            return 30;
+        //Implementar función que retorne el valor anterior
+        env.storage()
+            .persistent()
+            .get::<Symbol, i128>(&RESULTADO)
+            .unwrap_or(0);
+        return 30;
     }
 }
 
